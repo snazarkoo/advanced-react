b1821a1a825554f37ceff9b2c703a94aacf7be02
Nazar Shcherbiak <snazarkoo@gmail.com>
2017-12-05 23:59:41 +0200

    Refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
 
 const config = {
-  entry: ['babel-polyfill', './lib/components/Index.js'],
+  resolve: {
+    modules: [
+      path.resolve('./lib'),
+      path.resolve('./node_modules'),
+    ]
+  },
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
     ]
   }
 };
